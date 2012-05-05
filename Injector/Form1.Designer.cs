1366274523f1453891bcc900df938e8f90fa427f
unknown <Shakil@cuet-same.(none)>
2012-05-05 09:35:25 +0600

    Init

---
+++ Injector/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInjectorData = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBeforeTag = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder Path : ";
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Location = new System.Drawing.Point(104, 23);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(596, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Injector Data : ";
+            // 
+            // txtInjectorData
+            // 
+            this.txtInjectorData.Location = new System.Drawing.Point(104, 75);
+            this.txtInjectorData.Multiline = true;
+            this.txtInjectorData.Name = "txtInjectorData";
+            this.txtInjectorData.Size = new System.Drawing.Size(596, 146);
+            this.txtInjectorData.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Injection Before Tag : ";
+            // 
+            // txtBeforeTag
+            // 
+            this.txtBeforeTag.Location = new System.Drawing.Point(104, 293);
+            this.txtBeforeTag.Name = "txtBeforeTag";
+            this.txtBeforeTag.Size = new System.Drawing.Size(596, 20);
+            this.txtBeforeTag.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 449);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(596, 142);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 619);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBeforeTag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInjectorData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInjectorData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBeforeTag;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Injector/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Injector
+{
+    public partial class Form1 : Form
+    {
+        int _fileCount = 0;
+        int _changeCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtFolderPath.Text.Trim() == "")
+                MessageBox.Show("Give Path!!!");
+            else if (txtInjectorData.Text.Trim() == "")
+                MessageBox.Show("Give Data!!!");
+            else if (txtBeforeTag.Text.Trim() == "")
+                MessageBox.Show("Give Tag!!!");
+            else
+            {
+                button1.Enabled = false;
+                Convert(txtFolderPath.Text.Trim(), txtBeforeTag.Text.Trim(), txtInjectorData.Text);
+                MessageBox.Show("Total File : " + _fileCount + " Total Change : " + _changeCount);
+                button1.Enabled = true;
+            }
+        }
+
+
+        private void Convert(string path, string tag, string data)
+        {
+
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                
+                foreach (string file in files)
+                {
+                    _fileCount++;
+                    string content = File.ReadAllText(file);
+                    int k = content.ToLower().IndexOf(tag.ToLower());
+                    if (k != -1)
+                    {
+                        content = content.Substring(0, k) + data + content.Substring(k, content.Length - k);
+                        File.WriteAllText(file, content);
+                        _changeCount++;
+                    }
+                    else
+                    {
+                        textBox1.Text += file + "\n";
+                    }
+                }
+
+                string[] folders = Directory.GetDirectories(path);
+                foreach (string folder in folders)
+                {
+                    Convert(folder, tag, data);
+                }
+            }
+        }
+    }
+}
